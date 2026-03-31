898624b9c69b08f5c86d156c31e2cea391cfc5f6
jlbuild <juliabuildbot@gmail.com>
2026-03-31 04:23:06 +0000

    MQLib_jll build 0.1.1+0

--- src/MQLib_jll.jl
+++ src/MQLib_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MQLib")
-JLLWrappers.@generate_main_file("MQLib", UUID("4dedf8fe-8d9a-5fb8-8563-19379e8d5c54"))
+JLLWrappers.@generate_main_file("MQLib", Base.UUID("4dedf8fe-8d9a-5fb8-8563-19379e8d5c54"))
 end  # module MQLib_jll
